bc36f841cd6cdf664263af37d9c6076b5c91331f
zjy <1160704409@qq.com>
2018-09-10 15:18:38 +0800

    test

---
+++ src/pages/example/exam2.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h2>欢迎来到例子2</h2>
+  </div>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,22 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    // 定义子组件的时候,子组件路径的前面不能加/,否则找不到路径
+    // 在页面中跳转的时候可以使用this.$router.push('example/exma1')
+    {
+      path:"/example",
+      component:()=>import('@/views/example/index'),
+      children:[
+        {
+          path:"exam1",
+          component:()=>import('@/views/example/exam1')
+        },
+        {
+          path:"exam2",
+          component:()=>import('@/views/example/exam2')
+        }
+      ]
     }
   ]
 })
