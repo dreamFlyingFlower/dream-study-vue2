3a0cf0bf29534209c180da42d5b08f0ebf5f7e48
飞花梦影 <582822832@qq.com>
2022-02-06 23:23:26 +0800

    study something

--- src/views/example/exam1.vue
+++ src/views/example/exam1.vue
@@ -1,5 +1,66 @@
 <template>
   <div>
     <h1>欢迎来到栗子1</h1>
+    <h1 @test.stop="testStop"></h1>
+    <input v-bind="testBind" v-model="testModel" />
+    <h1 v-for="(val, key, i) in testObj" :key="key" ref="h1">
+      {{ i }},{{ val }}
+    </h1>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      // v-bind绑定的值只能是从数据层到视图层,视图层不能改变数据层,是单项绑定
+      testBind: 1,
+      // v-model是双向绑定
+      testModel: 2,
+      testObj: {
+        key1: val1,
+        key2: val2,
+      },
+    };
+  },
+  created() {},
+  methods: {
+    testStop() {
+      console.log("stop组织冒泡");
+    },
+    testPrevent() {
+      console.log("prevent阻止默认事件");
+    },
+    testCapture() {
+      console.log("capture添加事件监听器时使用事件捕获模式");
+    },
+    testSelf() {
+      console.log("sefl只当事件在该元素本身触发时触发回调");
+    },
+    testOnce() {
+      console.log("只触发一次");
+    },
+    testRef(){
+      // ref是代表了整个DOM元素,所有被ref标识的DOM元素都会放到refs中,可以快速操作
+      // 如果ref标识的是一个组件,可以获得组件中所有的属性,方法等
+      console.log(this.$refs.h1);
+    },
+    testRoute(){
+      // 获得当前页面URL地址信息,参数信息
+      console.log(this.$route);
+      // 使用query获得路由URL中的参数,此时参数直接拼接在URL后,形式为ip:port/login?name=xxxx&age=xxx
+      console.log(this.$route.query.name);
+      console.log(this.$route.query.age);
+      // 使用params获得URL中的参数,参数是通过:(冒号)进行分割,形式为ip:port/login/12/飞仙
+      // 此时路由Router必须是完全匹配的模式,定义的path必须是/login/:id/:name,login后的参数个数必须完全相同,否则无法跳转
+      console.log(this.$route.params.id);
+      console.log(this.$route.params.name);
+      // 不管是用query还是params获得URL中的参数,参数都会显示在URL中,不能对参数进行保密
+    }
+  },
+  // 自定义私有过滤器
+  filters: {},
+  // 自定义私有指令
+  directives: {},
+};
+</script>
--- src/views/example/index.vue
+++ src/views/example/index.vue
@@ -1,12 +1,91 @@
 <template>
   <div id="example">
     <router-view></router-view>
+
+    <router-view name="header"></router-view>
+    <router-view name="nav"></router-view>
+    <router-view name="content"></router-view>
   </div>
 </template>
 <script>
 // 若是有子组件,且通过vue-roter跳转组件,则需要一个父页面,父页面必须有一个router-view标签来承载子页面
 // 而router/index.js中的写法也需要注意
 export default {
+  // 路由可以根据router-view的name属性进行匹配
+  // 下方的header,nav,content是3个组件,前面的key必须是字符串,和上方视图的router-view的name值对应
+  // {
+  //   path:"/",components:{
+  //     "default":header,
+  //     "header":header,
+  //     "nav":nav,
+  //     "content":content
+  //   }
+  // }
 
-}
+  // Vue生命周期
+  // 开始创建一个Vue实例对象
+  // new Vue();
+  // 初始化一个Vue空实例对象,此时,该对象身上只有默认的一些生命周期函数和默认事件,其他的未创建
+  // Init event & Lifecycle
+  // Vue生命周期遇到的第一个生命周期函数,实例完全被创建之后会调用
+  // 该钩子被调用之前,data和methods中的数据还没有初始化
+  beforeCreate() {},
+  // data和methods注入
+  // Init Injections & reactivity
+  // 该钩子函数被调用时,data和methods已经初始化完毕
+  created() {},
+  // Vue在调用该钩子函数之前,会将页面,数据模板加载到内存中并完成必要的赋值等操作,但是还没渲染到页面中
+  // 在beforeMount执行的时候,页面中的元素,还没有被真正的替换完成,仍然是一些模板字符串,这就会导致{{}}这种操作会有短暂的不协调
+  beforeMount() {},
+  // 此时内存中的模板已经渲染到页面中,Vue实例也已经完成创建好了
+  // 到此时,正常的Vue声明周期已经完成
+  mounted() {},
+  // 以下是运行时钩子函数
+  // data中的数据没有被更改刷新之前的操作,数据仍然是旧的
+  beforeUpdate() {},
+  // 数据更新之后的操作,此时数据已经是最新的了
+  updated() {},
+  // Vue实例即将被销毁.此时data和methods中的数据和方法仍然可用
+  beforeDestroy() {},
+  // 组件完全被销毁
+  destroyed() {},
+  // 组件数据,必须是一个方法,并且返回一个对象
+  data() {
+    return {};
+  },
+  // 引用外部组件,自定义内部组件
+  components: {
+    login: {
+      template: "<h1>这是一个私有的自定义组件,{{msg}}</h1>",
+      // 该data和外部的data作用一样,可以在内部组件使用
+      data() {
+        return {
+          msg: "这是自定义组件内部的数据,和外部组件数据可互通",
+        };
+      },
+    },
+  },
+  watch: {
+    // 监听路由
+    "$route.path": function (newVal, oldVal) {},
+  },
+  methods: {
+    // 利用axios发送ajax请求,用到比较少的合并请求
+    merge: function () {
+      this.$axios
+        // 合并请求,要么都成功,要么都失败
+        .all([this.$axios.get(), this.$axios.post()])
+        // 处理结果,根据顺序对应结果,有多少请求就有多少结果
+        .then(
+          this.$axios.spread((res1, res2) => {
+            console.log(res1);
+            console.log(res2);
+          })
+        )
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+};
 </script>
